6fe5a5193cead3550f5d7ba467c91f017cfaf0b9
Gabriel Cebrian <gabceb@gmail.com>
2013-11-13 12:00:56 -0800

    Added tests for the new name property

--- test/test.js
+++ test/test.js
@@ -7,21 +7,24 @@ ua = {
         android: "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Mobile Safari/537.36",
         linux: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36",
 		version : "32.0.1664.3",
-        versionNumber : 32
+        versionNumber : 32,
+        name : "chrome"
 	},
 	safari : {
 		mac: "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9) AppleWebKit/537.71 (KHTML, like Gecko) Version/7.0 Safari/537.71",
 		ipad: "Mozilla/5.0(iPad; U; CPU iPhone OS 7_0_3 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11B508 Safari/9537.53",
 		iphone: "Mozilla/5.0(iPhone; CPU iPhone OS 7_0_3 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11B508 Safari/9537.53",
 		version: "7.0",
-        versionNumber: 7
+        versionNumber: 7,
+        name : "safari"
 	},
 	firefox : {
 		windows: "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0",
 		mac: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:25.0) Gecko/20100101 Firefox/25.0",
         linux: "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:25.0) Gecko/20100101 Firefox/25.0",
 		version: "25.0",
-        versionNumber: 25
+        versionNumber: 25,
+        name : "firefox"
 	},
 	ie : {
         windows : {
@@ -31,7 +34,8 @@ ua = {
         },
         win_phone : {
             v_10: "Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 1020)"
-        }
+        },
+        name : "msie"
 	},
   opera : {
     v_15: {
@@ -45,7 +49,8 @@ ua = {
       windows: "Opera/9.80 (Windows NT 6.1; U; en) Presto/2.6.30 Version/10.00",
       version: "10.00",
       versionNumber: 10
-    }
+    },
+    name : "opera"
   }
 }
 
@@ -53,7 +58,7 @@ test_url = "https://www.blendspace.com"
 
 util.dump("Casper tests running using " + test_url);
 
-casper.test.begin("when using Chrome on Windows", 5, function(test) {
+casper.test.begin("when using Chrome on Windows", 6, function(test) {
   casper.userAgent(ua.chrome.windows);
 
   casper.start(test_url).then(function(){
@@ -63,6 +68,8 @@ casper.test.begin("when using Chrome on Windows", 5, function(test) {
   	});
   	
   	test.assert(browser.chrome, "Browser should be Chrome");
+    test.assertEquals(browser.name, ua.chrome.name,"Browser name should be " + ua.chrome.name);
+
   	test.assert(browser.webkit, "Browser should be webkit based");
   	test.assertEquals(browser.version, ua.chrome.version, "String version should be " + ua.chrome.version);
     test.assertEquals(browser.versionNumber, ua.chrome.versionNumber, "Number version should be " + ua.chrome.versionNumber);
@@ -74,7 +81,7 @@ casper.test.begin("when using Chrome on Windows", 5, function(test) {
   });
 });
 
-casper.test.begin("when using Chrome on Mac", 5, function(test) {
+casper.test.begin("when using Chrome on Mac", 6, function(test) {
   casper.userAgent(ua.chrome.mac);
 
   casper.start(test_url).then(function(){
@@ -84,6 +91,8 @@ casper.test.begin("when using Chrome on Mac", 5, function(test) {
   	});
   	
   	test.assert(browser.chrome, "Browser should be Chrome");
+    test.assertEquals(browser.name, ua.chrome.name,"Browser name should be " + ua.chrome.name);
+
   	test.assert(browser.webkit, "Browser should be webkit based");
   	test.assertEquals(browser.version, ua.chrome.version, "Version should be " + ua.chrome.version);
     test.assertEquals(browser.versionNumber, ua.chrome.versionNumber, "Number version should be " + ua.chrome.versionNumber);
@@ -95,7 +104,7 @@ casper.test.begin("when using Chrome on Mac", 5, function(test) {
   });
 });
 
-casper.test.begin("when using Chrome on an Android device", 5, function(test) {
+casper.test.begin("when using Chrome on an Android device", 6, function(test) {
   casper.userAgent(ua.chrome.android);
 
   casper.start(test_url).then(function(){
@@ -105,6 +114,8 @@ casper.test.begin("when using Chrome on an Android device", 5, function(test) {
     });
     
     test.assert(browser.chrome, "Browser should be Chrome");
+    test.assertEquals(browser.name, ua.chrome.name,"Browser name should be " + ua.chrome.name);
+
     test.assert(browser.webkit, "Browser should be webkit based");
     test.assertEquals(browser.version, ua.chrome.version, "Version should be " + ua.chrome.version);
     test.assertEquals(browser.versionNumber, ua.chrome.versionNumber, "Version should be " + ua.chrome.versionNumber);
@@ -116,7 +127,7 @@ casper.test.begin("when using Chrome on an Android device", 5, function(test) {
   });
 });
 
-casper.test.begin("when using Chrome on Linux", 5, function(test) {
+casper.test.begin("when using Chrome on Linux", 6, function(test) {
   casper.userAgent(ua.chrome.linux);
 
   casper.start(test_url).then(function(){
@@ -126,6 +137,8 @@ casper.test.begin("when using Chrome on Linux", 5, function(test) {
     });
     
     test.assert(browser.chrome, "Browser should be Chrome");
+    test.assertEquals(browser.name, ua.chrome.name,"Browser name should be " + ua.chrome.name);
+
     test.assert(browser.webkit, "Browser should be webkit based");
     test.assertEquals(browser.version, ua.chrome.version, "Version should be " + ua.chrome.version);
     test.assertEquals(browser.versionNumber, ua.chrome.versionNumber, "Version should be " + ua.chrome.versionNumber);
@@ -137,7 +150,7 @@ casper.test.begin("when using Chrome on Linux", 5, function(test) {
   });
 });
 
-casper.test.begin("when using Firefox on Windows", 5, function(test) {
+casper.test.begin("when using Firefox on Windows", 6, function(test) {
   casper.userAgent(ua.firefox.windows);
 
   casper.start(test_url).then(function(){
@@ -147,6 +160,8 @@ casper.test.begin("when using Firefox on Windows", 5, function(test) {
   	});
 
   	test.assert(browser.mozilla, "Browser should be Mozilla");
+    test.assertEquals(browser.name, ua.firefox.name,"Browser name should be " + ua.firefox.name);
+
   	test.assertEquals(browser.version, ua.firefox.version, "Version should be " + ua.firefox.version);
     test.assertEquals(browser.versionNumber, ua.firefox.versionNumber, "Version should be " + ua.firefox.versionNumber);
   	test.assert(browser.win, "Platform should be Windows");
@@ -158,7 +173,7 @@ casper.test.begin("when using Firefox on Windows", 5, function(test) {
   });
 });
 
-casper.test.begin("when using Firefox on Mac", 5, function(test) {
+casper.test.begin("when using Firefox on Mac", 6, function(test) {
   casper.userAgent(ua.firefox.mac);
 
   casper.start(test_url).then(function(){
@@ -168,6 +183,8 @@ casper.test.begin("when using Firefox on Mac", 5, function(test) {
   	});
   	
   	test.assert(browser.mozilla, "Browser should be Mozilla");
+    test.assertEquals(browser.name, ua.firefox.name,"Browser name should be " + ua.firefox.name);
+
   	test.assertEquals(browser.version, ua.firefox.version, "Version should be " + ua.firefox.version);
     test.assertEquals(browser.versionNumber, ua.firefox.versionNumber, "Version should be " + ua.firefox.versionNumber);
   	test.assert(browser.mac, "Platform should be mac");
@@ -179,7 +196,7 @@ casper.test.begin("when using Firefox on Mac", 5, function(test) {
   });
 });
 
-casper.test.begin("when using Firefox on Linux", 5, function(test) {
+casper.test.begin("when using Firefox on Linux", 6, function(test) {
   casper.userAgent(ua.firefox.linux);
 
   casper.start(test_url).then(function(){
@@ -189,6 +206,8 @@ casper.test.begin("when using Firefox on Linux", 5, function(test) {
     });
     
     test.assert(browser.mozilla, "Browser should be Mozilla");
+    test.assertEquals(browser.name, ua.firefox.name,"Browser name should be " + ua.firefox.name);
+
     test.assertEquals(browser.version, ua.firefox.version, "Version should be " + ua.firefox.version);
     test.assertEquals(browser.versionNumber, ua.firefox.versionNumber, "Version should be " + ua.firefox.versionNumber);
     test.assert(browser.linux, "Platform should be linux");
@@ -200,7 +219,7 @@ casper.test.begin("when using Firefox on Linux", 5, function(test) {
   });
 });
 
-casper.test.begin("when using Safari on Mac", 5, function(test) {
+casper.test.begin("when using Safari on Mac", 6, function(test) {
   casper.userAgent(ua.safari.mac);
 
   casper.start(test_url).then(function(){
@@ -210,6 +229,8 @@ casper.test.begin("when using Safari on Mac", 5, function(test) {
   	});
   	
   	test.assert(browser.safari, "Browser should be Safari");
+    test.assertEquals(browser.name, ua.safari.name,"Browser name should be " + ua.safari.name);
+
   	test.assert(browser.webkit, "Browser should be webkit based");
   	test.assertEquals(browser.version, ua.safari.version, "Version should be " + ua.safari.version);
     test.assertEquals(browser.versionNumber, ua.safari.versionNumber, "Version should be " + ua.safari.versionNumber);
@@ -221,7 +242,7 @@ casper.test.begin("when using Safari on Mac", 5, function(test) {
   });
 });
 
-casper.test.begin("when using Safari on iPad", 5, function(test) {
+casper.test.begin("when using Safari on iPad", 6, function(test) {
   casper.userAgent(ua.safari.ipad);
 
   casper.start(test_url).then(function(){
@@ -231,6 +252,8 @@ casper.test.begin("when using Safari on iPad", 5, function(test) {
   	});
   	
   	test.assert(browser.safari, "Browser should be Safari");
+    test.assertEquals(browser.name, ua.safari.name,"Browser name should be " + ua.safari.name);
+
   	test.assert(browser.webkit, "Browser should be webkit based");
   	test.assertEquals(browser.version, ua.safari.version, "Version should be " + ua.safari.version);
     test.assertEquals(browser.versionNumber, ua.safari.versionNumber, "Version number should be " + ua.safari.versionNumber);
@@ -242,7 +265,7 @@ casper.test.begin("when using Safari on iPad", 5, function(test) {
   });
 });
 
-casper.test.begin("when using Safari on iPhone", 5, function(test) {
+casper.test.begin("when using Safari on iPhone", 6, function(test) {
   casper.userAgent(ua.safari.iphone);
 
   casper.start(test_url).then(function(){
@@ -252,6 +275,8 @@ casper.test.begin("when using Safari on iPhone", 5, function(test) {
   	});
   	
   	test.assert(browser.safari, "Browser should be Safari");
+    test.assertEquals(browser.name, ua.safari.name,"Browser name should be " + ua.safari.name);
+
   	test.assert(browser.webkit, "Browser should be webkit based");
   	test.assertEquals(browser.version, ua.safari.version, "Version should be " + ua.safari.version);
     test.assertEquals(browser.versionNumber, ua.safari.versionNumber, "Version number should be " + ua.safari.versionNumber);
@@ -263,7 +288,7 @@ casper.test.begin("when using Safari on iPhone", 5, function(test) {
   });
 });
 
-casper.test.begin("when using IE9", 5, function(test) {
+casper.test.begin("when using IE9", 6, function(test) {
   casper.userAgent(ua.ie.windows.v_9);
 
   casper.start(test_url).then(function(){
@@ -273,6 +298,8 @@ casper.test.begin("when using IE9", 5, function(test) {
   	});
   	
   	test.assert(browser.msie, "Browser should be IE");
+    test.assertEquals(browser.name, ua.ie.name,"Browser name should be " + ua.ie.name);
+
   	test.assertEquals(browser.version, "9.0", "Version should be 9.0");
     test.assertEquals(browser.versionNumber, 9, "Version should be 9");
   	test.assert(browser.win, "Platform should be Windows");
@@ -284,7 +311,7 @@ casper.test.begin("when using IE9", 5, function(test) {
   });
 });
 
-casper.test.begin("when using IE10", 5, function(test) {
+casper.test.begin("when using IE10", 6, function(test) {
   casper.userAgent(ua.ie.windows.v_10);
 
   casper.start(test_url).then(function(){
@@ -294,6 +321,8 @@ casper.test.begin("when using IE10", 5, function(test) {
   	});
   	
   	test.assert(browser.msie, "Browser should be IE");
+    test.assertEquals(browser.name, ua.ie.name,"Browser name should be " + ua.ie.name);
+
   	test.assertEquals(browser.version, "10.0", "Version should be 10");
     test.assertEquals(browser.versionNumber, 10, "Version should be 10");
   	test.assert(browser.win, "Platform should be Windows");
@@ -305,7 +334,7 @@ casper.test.begin("when using IE10", 5, function(test) {
   });
 });
 
-casper.test.begin("when using IE11", 5, function(test) {
+casper.test.begin("when using IE11", 6, function(test) {
   casper.userAgent(ua.ie.windows.v_11);
 
   casper.start(test_url).then(function(){
@@ -315,6 +344,8 @@ casper.test.begin("when using IE11", 5, function(test) {
   	});
   	
   	test.assert(browser.msie, "Browser should be IE");
+    test.assertEquals(browser.name, ua.ie.name,"Browser name should be " + ua.ie.name);
+
   	test.assertEquals(browser.version, "11.0", "Version should be 11.0");
     test.assertEquals(browser.versionNumber, 11, "Version should be 11");
   	test.assert(browser.win, "Platform should be Windows");
@@ -326,7 +357,7 @@ casper.test.begin("when using IE11", 5, function(test) {
   });
 });
 
-casper.test.begin("when using IE10 on a Windows Phone", 5, function(test) {
+casper.test.begin("when using IE10 on a Windows Phone", 6, function(test) {
   casper.userAgent(ua.ie.win_phone.v_10);
 
   casper.start(test_url).then(function(){
@@ -336,6 +367,8 @@ casper.test.begin("when using IE10 on a Windows Phone", 5, function(test) {
     });
     
     test.assert(browser.msie, "Browser should be IE");
+    test.assertEquals(browser.name, ua.ie.name,"Browser name should be " + ua.ie.name);
+
     test.assertEquals(browser.version, "10.0", "Version should be 10.0");
     test.assertEquals(browser.versionNumber, 10, "Version should be 10");
     test.assert(browser["windows phone"], "Platform should be Windows Phone");
@@ -347,7 +380,7 @@ casper.test.begin("when using IE10 on a Windows Phone", 5, function(test) {
   });
 });
 
-casper.test.begin("when using Opera 15+ on Windows", 5, function(test) {
+casper.test.begin("when using Opera 15+ on Windows", 6, function(test) {
   casper.userAgent(ua.opera.v_15.windows);
 
   casper.start(test_url).then(function(){
@@ -357,6 +390,8 @@ casper.test.begin("when using Opera 15+ on Windows", 5, function(test) {
     });
     
     test.assert(browser.opera, "Browser should be Opera");
+    test.assertEquals(browser.name, ua.opera.name,"Browser name should be " + ua.opera.name);
+
     test.assertEquals(browser.version, ua.opera.v_15.version, "Version should be " + ua.opera.v_15.version);
     test.assertEquals(browser.versionNumber, ua.opera.v_15.versionNumber, "Version number should be " + ua.opera.v_15.versionNumber);
     test.assert(browser.win, "Platform should be Windows");
@@ -368,7 +403,7 @@ casper.test.begin("when using Opera 15+ on Windows", 5, function(test) {
   });
 });
 
-casper.test.begin("when using Opera 15+ on Mac", 5, function(test) {
+casper.test.begin("when using Opera 15+ on Mac", 6, function(test) {
   casper.userAgent(ua.opera.v_15.mac);
 
   casper.start(test_url).then(function(){
@@ -378,6 +413,8 @@ casper.test.begin("when using Opera 15+ on Mac", 5, function(test) {
     });
     
     test.assert(browser.opera, "Browser should be Opera");
+    test.assertEquals(browser.name, ua.opera.name,"Browser name should be " + ua.opera.name);
+
     test.assertEquals(browser.version, ua.opera.v_15.version, "Version should be " + ua.opera.v_15.version)
     test.assertEquals(browser.versionNumber, ua.opera.v_15.versionNumber, "Version number should be " + ua.opera.v_15.versionNumber);
     test.assert(browser.mac, "Platform should be Mac");
@@ -389,7 +426,7 @@ casper.test.begin("when using Opera 15+ on Mac", 5, function(test) {
   });
 });
 
-casper.test.begin("when using Opera 10 on Windows", 5, function(test) {
+casper.test.begin("when using Opera 10 on Windows", 6, function(test) {
   casper.userAgent(ua.opera.v_10.windows);
 
   casper.start(test_url).then(function(){
@@ -399,6 +436,8 @@ casper.test.begin("when using Opera 10 on Windows", 5, function(test) {
     });
     
     test.assert(browser.opera, "Browser should be Opera");
+    test.assertEquals(browser.name, ua.opera.name,"Browser name should be " + ua.opera.name);
+
     test.assertEquals(browser.version, ua.opera.v_10.version, "Version should be " + ua.opera.v_10.version);
     test.assertEquals(browser.versionNumber, ua.opera.v_10.versionNumber, "Version number should be " + ua.opera.v_10.versionNumber);
     test.assert(browser.win, "Platform should be Windows");
@@ -410,7 +449,7 @@ casper.test.begin("when using Opera 10 on Windows", 5, function(test) {
   });
 });
 
-casper.test.begin("when using Opera 10 on Mac", 5, function(test) {
+casper.test.begin("when using Opera 10 on Mac", 6, function(test) {
   casper.userAgent(ua.opera.v_10.mac);
 
   casper.start(test_url).then(function(){
@@ -420,6 +459,8 @@ casper.test.begin("when using Opera 10 on Mac", 5, function(test) {
     });
     
     test.assert(browser.opera, "Browser should be Opera");
+    test.assertEquals(browser.name, ua.opera.name,"Browser name should be " + ua.opera.name);
+    
     test.assertEquals(browser.version, ua.opera.v_10.version, "Version should be " + ua.opera.v_10.version);
     test.assertEquals(browser.versionNumber, ua.opera.v_10.versionNumber, "Version number should be " + ua.opera.v_10.versionNumber);
     test.assert(browser.mac, "Platform should be Mac");
