07919f3a6e73d1a46582669066e763a47d43eacb
Gabriel Cebrian <gabceb@gmail.com>
2013-11-14 21:18:56 -0800

    Added uglify task to grunt Updated min file to uglified version Moved js files to /dist folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) { 
 	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 		jshint: {
 			files: ['gruntfile.js', 'jquery.browser.js'],
 
@@ -10,10 +11,21 @@ module.exports = function(grunt) {
 					module: true
 				}
 			}
+		},
+		uglify: {
+			options: {
+				banner: '/*!\n * jQuery Browser Plugin <%= pkg.version %>\n * https://github.com/gabceb/jquery-browser-plugin\n *\n * Original jquery-browser code Copyright 2005, 2013 jQuery Foundation, Inc. and other contributors\n * http://jquery.org/license\n *\n * Modifications Copyright <%= grunt.template.today("yyyy") %> Gabriel Cebrian\n * https://github.com/gabceb\n *\n * Released under the MIT license\n *\n * Date: <%= grunt.template.today("dd-mm-yyyy")%>\n */'
+			},
+			dist: {
+				files: {
+					'dist/<%= pkg.name %>.min.js': 'dist/<%= pkg.name %>.js'
+				}
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	grunt.registerTask('default', ['jshint']);
+	grunt.registerTask('default', ['jshint', 'uglify']);
 };
