4bb46ae5018e40a3d19745a21558cf3c7306a915
Triangle717 <le717.code@yahoo.com>
2014-12-12 12:02:52 -0500

    Restore and improve ordering of `match` and `platform_match`, fix incorrect identification of all Blackberry and Kindle browers

--- dist/jquery.browser.js
+++ dist/jquery.browser.js
@@ -31,32 +31,32 @@
   jQuery.uaMatch = function( ua ) {
     ua = ua.toLowerCase();
 
-    var match = /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
-        /(edge)\/([\w.]+)/.exec( ua ) ||
-        /(msie) ([\w.]+)/.exec( ua ) ||
-        /(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
+    var match = /(edge)\/([\w.]+)/.exec( ua ) ||
         /(opr)[\/]([\w.]+)/.exec( ua ) ||
+        /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
         /(version)(applewebkit)[ \/]([\w.]+).*(safari)[ \/]([\w.]+)/.exec( ua ) ||
         /(webkit)[ \/]([\w.]+).*(version)[ \/]([\w.]+).*(safari)[ \/]([\w.]+)/.exec( ua ) ||
         /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
+        /(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
+        /(msie) ([\w.]+)/.exec( ua ) ||
         ua.indexOf("trident") >= 0 && /(rv)(?::| )([\w.]+)/.exec( ua ) ||
         ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
         [];
 
-    var platform_match = /(android)/.exec( ua ) ||
+    var platform_match = /(ipad)/.exec( ua ) ||
+        /(ipod)/.exec( ua ) ||
+        /(iphone)/.exec( ua ) ||
+        /(kindle)/.exec( ua ) ||
+        /(silk)/.exec( ua ) ||
+        /(android)/.exec( ua ) ||
+        /(windows phone)/.exec( ua ) ||
+        /(win)/.exec( ua ) ||
+        /(mac)/.exec( ua ) ||
+        /(linux)/.exec( ua ) ||
+        /(cros)/.exec( ua ) ||
+        /(playbook)/.exec( ua ) ||
         /(bb)/.exec( ua ) ||
         /(blackberry)/.exec( ua ) ||
-        /(cros)/.exec( ua ) ||
-        /(ipad)/.exec( ua ) ||
-        /(iphone)/.exec( ua ) ||
-        /(ipod)/.exec( ua ) ||
-        /(kindle)/.exec( ua ) ||
-        /(linux)/.exec( ua ) ||
-        /(mac)/.exec( ua ) ||
-        /(playbook)/.exec( ua ) ||
-        /(silk)/.exec( ua ) ||
-        /(win)/.exec( ua ) ||
-        /(windows phone)/.exec( ua ) ||
         [];
 
     return {
@@ -105,12 +105,28 @@
     browser[ie] = true;
   }
 
-  // BB10 is a newer OS version of BlackBerry
-  if ( browser.bb ) {
+  // Blackberry browsers are marked as Safari on BlackBerry
+  if ( browser.safari && browser.blackberry ) {
     var blackberry = "blackberry";
 
     matched.browser = blackberry;
     browser[blackberry] = true;
+  }
+
+  // Playbook browsers are marked as Safari on Playbook
+  if ( browser.safari && browser.playbook ) {
+    var playbook = "playbook";
+
+    matched.browser = playbook;
+    browser[playbook] = true;
+  }
+
+  // BB10 is a newer OS version of BlackBerry
+  if ( browser.bb ) {
+    var bb = "blackberry";
+
+    matched.browser = bb;
+    browser[bb] = true;
   }
 
   // Opera 15+ are identified as opr
@@ -127,6 +143,22 @@
 
     matched.browser = android;
     browser[android] = true;
+  }
+
+  // Kindle browsers are marked as Safari on Kindle
+  if ( browser.safari && browser.kindle ) {
+    var kindle = "kindle";
+
+    matched.browser = kindle;
+    browser[kindle] = true;
+  }
+
+   // Kindle Silk browsers are marked as Safari on Kindle
+  if ( browser.safari && browser.silk ) {
+    var silk = "silk";
+
+    matched.browser = silk;
+    browser[silk] = true;
   }
 
   // Assign the name and platform variable
