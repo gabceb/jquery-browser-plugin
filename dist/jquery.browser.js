07d59ee6e87da9f789b196d221e5f62f9db3c158
Gabriel Cebrian <gabceb@gmail.com>
2015-11-23 11:56:22 -0800

    Bump the minor

--- dist/jquery.browser.js
+++ dist/jquery.browser.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery Browser Plugin 0.0.8
+ * jQuery Browser Plugin 0.1.0
  * https://github.com/gabceb/jquery-browser-plugin
  *
  * Original jquery-browser code Copyright 2005, 2015 jQuery Foundation, Inc. and other contributors
